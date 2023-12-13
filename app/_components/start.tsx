0999c613e6a0879ecaef630d58fb2bd1396ccad0
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2023-12-12 18:21:46 -0800

    Project Card + parallax effect

--- app/_components/start.tsx
+++ app/_components/start.tsx
@@ -9,39 +9,32 @@ function Start() {
 
 	return (
 		<header id="start">
-			{/* <div className="min-h-screen w-screen bg-start-page bg-cover top-0 left-0 overflow-hidden flex justify-center items-end z-0"> */}
-			<div className="min-h-screen w-screen bg-start-page bg-cover relative overflow-hidden flex flex-wrap justify-between items-center">
-				<div className="flex flex-wrap -mx-4 self-center w-full items-center content-center">
-					{/* <div className="flex items-start"> */}
+			<div className="min-h-screen w-screen bg-start-page bg-cover flex items-center">
+				<div className="mx-auto relative w-7/12">
+					<div className="timeline">
+						<div className="bullet"></div>
+					</div>
 
-					<div className="mx-auto sm:col-span-7 md:col-span-7 col-span-9 relative">
-						<div className="timeline">
-							<div className="bullet"></div>
-						</div>
+					<div className="text-primary space-y-4 m-4">
+						<small className="text-primaryGray">
+							Start<span className="ml-1">/&gt;</span>
+						</small>
 
-						<div className="text-primary text-4xl font-bold space-y-4 mx-4">
-							<small className="text-primaryGray text-lg font-light ">
-								Start<span className="ml-1">/&gt;</span>
-							</small>
+						<h1>
+							Hi, my name is{' '}
+							<strong className="text-purple font-extrabold">
+								Ashkan Rahnavardi
+							</strong>
+							<br></br>
+						</h1>
 
-							<h1>
-								Hi, my name is{' '}
-								<strong className="text-purple font-extrabold">
-									Ashkan Rahnavardi
-								</strong>
-								<br></br>
-							</h1>
+						<h1>
+							I <em className="font-serif font-normal">design</em> and{' '}
+							<span className="font-mono font-bold">develop</span>{' '}
+							{scrambledText}
+						</h1>
 
-							<h1>
-								I <em className="font-serif font-normal">design</em> and{' '}
-								<span className="font-mono font-bold">develop</span>{' '}
-								{scrambledText}
-							</h1>
-
-							<p className="text-primaryGray text-2xl font-normal">
-								Let me show You...
-							</p>
-						</div>
+						<p className="text-primaryGray">Let me show You...</p>
 					</div>
 				</div>
 			</div>
--- app/_components/work.tsx
+++ app/_components/work.tsx
@@ -1,28 +1,73 @@
+'use client';
+
+import Tilt from 'react-parallax-tilt';
+
 function Work() {
+	const ParallaxEffect = () => (
+		<Tilt className="parallax-effect" perspective={500}>
+			<div className="inner-element">
+				<div>React</div>
+
+				<div>Parallax Tilt</div>
+
+				<div>👀</div>
+			</div>
+		</Tilt>
+	);
+
+	const ProjectCard = ({
+		imageUrl,
+		className,
+	}: {
+		imageUrl: string;
+		className: string;
+	}) => {
+		return (
+			// <Tilt>
+			<Tilt
+				perspective={500}
+				glareEnable={true}
+				glareMaxOpacity={0.45}
+				scale={1.02}
+				className={'w-10/12 h-56 relative z-10 parallax-effect ' + className}
+				style={{
+					backgroundImage: `url(${imageUrl})`,
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+				}}
+			>
+				<div className="absolute bottom-0 -left-16 text-primary w-36 inner-element">
+					<h3>Plate</h3>
+					<h3>Grabber</h3>
+					<div className="w-full h-1 bg-primary my-2"></div>
+					<p className="text-primaryGray font-mono text-lg">00</p>
+					<p className="text-primaryGray font-mono text-lg">→</p>
+				</div>
+			</Tilt>
+			// </Tilt>
+		);
+	};
+
 	return (
-		<div
-			id="work"
-			className="min-h-screen flex flex-wrap justify-center items-start"
-		>
-			<div className="flex flex-wrap -mx-4 self-top w-full items-start justify-center">
-				<div className="mx-auto sm:col-span-7 md:col-span-7 col-span-9 relative pt-0">
-					<div className="timeline alt"></div>
-					<small className="section-title bullet-title alt">Work /&gt;</small>
-					<h4 id="work-titles">Selected web, mobile, video projects...</h4>
+		<div id="work" className="min-h-screen w-screen overflow-hidden flex ">
+			<div className="mx-auto relative w-7/12">
+				<div className="timeline">
+					<div className="bullet"></div>
+				</div>
+
+				<div className="text-primaryGray space-y-4 m-4">
+					<small>
+						Work<span className="ml-1">/&gt;</span>
+					</small>
+					<h2>Selected full-stack projects...</h2>
+				</div>
+
+				<div className="mx-4 my-8">
+					<ProjectCard imageUrl="/bc-plate2.png" className="-left-14" />
+					{/* <ParallaxEffect /> */}
 				</div>
 			</div>
 		</div>
-		// <div id="work" className="min-h-screen w-screen relative overflow-hidden flex flex-wrap justify-between items-center">
-		// 	<div className="flex flex-wrap -mx-4 self-center w-full items-center content-center">
-		// 		{/* <div className="flex items-start"> */}
-
-		// 		<div className="mx-auto sm:col-span-7 md:col-span-7 col-span-9 relative">
-		// 			<div className="timeline">
-		// 				<div className="bullet"></div>
-		// 			</div>
-		// 		</div>
-		// 	</div>
-		// </div>
 	);
 }
 
