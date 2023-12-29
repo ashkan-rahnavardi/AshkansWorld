605424380b698a468d204306656bc61447f84f03
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2023-12-29 10:21:45 -0800

    fixed font size issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 				<title>ASH</title>
 			</head>
 			<body className={inter.className}>
