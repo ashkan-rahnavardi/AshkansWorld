0a82c01b54683244302482324dfe125b84c04424
Ashkan Rahnavardi <ashkan.rahnavardi@gmail.com>
2025-06-05 16:12:04 -0700

    update about me section

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,107 +1,107 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useState } from 'react';
+import Image from "next/image";
+import { useState } from "react";
 
 const Navbar: React.FC = () => {
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-	const toggleMenu = () => {
-		setIsMenuOpen(!isMenuOpen);
-	};
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-	const closeMenu = () => {
-		setIsMenuOpen(false);
-	};
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
-	return (
-		<nav className="p-4 absolute z-10 bg-transparent w-full">
-			<div className="mx-auto flex justify-between items-center relative z-10">
-				{/* Logo */}
-				<a href="/" id="logo" className="ml-2">
-					<Image
-						src="/ash.png"
-						alt="Logo"
-						width={170}
-						height={32}
-						className="relative z-10"
-					/>
-				</a>
+  return (
+    <nav className="p-4 absolute z-10 bg-transparent w-full">
+      <div className="mx-auto flex justify-between items-center relative z-10">
+        {/* Logo */}
+        <a href="/" id="logo" className="ml-2">
+          <Image
+            src="/ash.png"
+            alt="Logo"
+            width={170}
+            height={32}
+            className="relative z-10"
+          />
+        </a>
 
-				{/* Navigation Links */}
-				<div className="hidden md:flex space-x-4">
-					<a href="#start" className="navbar-item hover:text-primary">
-						Start<span className="text-primaryDark ml-1">/&gt;</span>
-					</a>
-					<a href="#work" className="navbar-item hover:text-primary">
-						Work<span className="text-primaryDark ml-1">/&gt;</span>
-					</a>
-					<a href="#about" className="navbar-item hover:text-primary">
-						About<span className="textyDark ml-1">/&gt;</span>
-					</a>
-					<a href="#contact" className="navbar-item hover:text-primary">
-						Contact<span className="text-primaryDark ml-1">/&gt;</span>
-					</a>
-				</div>
+        {/* Navigation Links */}
+        <div className="hidden md:flex space-x-4">
+          <a href="#start" className="navbar-item hover:text-primary">
+            Start<span className="text-primaryDark ml-1">/&gt;</span>
+          </a>
+          <a href="#work" className="navbar-item hover:text-primary">
+            Work<span className="text-primaryDark ml-1">/&gt;</span>
+          </a>
+          <a href="#about" className="navbar-item hover:text-primary">
+            About<span className="textyDark ml-1">/&gt;</span>
+          </a>
+          <a href="#contact" className="navbar-item hover:text-primary">
+            Contact<span className="text-primaryDark ml-1">/&gt;</span>
+          </a>
+        </div>
 
-				{/* Mobile Menu Icon */}
-				<div className="md:hidden relative z-10">
-					<button onClick={toggleMenu} className="text-white">
-						<svg
-							className="w-12 h-12"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M4 6h16M4 12h16m-7 6h7"
-							></path>
-						</svg>
-					</button>
-				</div>
+        {/* Mobile Menu Icon */}
+        <div className="md:hidden relative z-10">
+          <button onClick={toggleMenu} className="text-white">
+            <svg
+              className="w-12 h-12"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
 
-				{/* Mobile Menu Dropdown */}
-				<div
-					className={`${
-						isMenuOpen ? 'max-h-full' : 'max-h-0 invisible'
-					} md:hidden fixed top-16 right-0 left-0 text-center p-4 transition-all duration-300 ease-in-out overflow-hidden navbar-dropdown z-0`}
-				>
-					<a
-						onClick={closeMenu}
-						href="#start"
-						className="block navbar-item hover:text-primary py-2"
-					>
-						Start<span className="text-primaryDark ml-1">/&gt;</span>
-					</a>
-					<a
-						href="#work"
-						className="block navbar-item hover:text-primary py-2"
-						onClick={closeMenu}
-					>
-						Work<span className="text-primaryDark ml-1">/&gt;</span>
-					</a>
-					<a
-						href="#about"
-						className="block navbar-item hover:text-primary py-2"
-						onClick={closeMenu}
-					>
-						About<span className="textyDark ml-1">/&gt;</span>
-					</a>
-					<a
-						href="#contact"
-						className="block navbar-item hover:text-primary py-2 mb-4"
-						onClick={closeMenu}
-					>
-						Contact<span className="text-primaryDark ml-1">/&gt;</span>
-					</a>
-				</div>
-			</div>
-		</nav>
-	);
+        {/* Mobile Menu Dropdown */}
+        <div
+          className={`${
+            isMenuOpen ? "max-h-full" : "max-h-0 invisible"
+          } md:hidden fixed top-16 right-0 left-0 text-center p-4 transition-all duration-300 ease-in-out overflow-hidden navbar-dropdown z-0`}
+        >
+          <a
+            onClick={closeMenu}
+            href="#start"
+            className="block navbar-item hover:text-primary py-2"
+          >
+            Start<span className="text-primaryDark ml-1">/&gt;</span>
+          </a>
+          <a
+            href="#work"
+            className="block navbar-item hover:text-primary py-2"
+            onClick={closeMenu}
+          >
+            Work<span className="text-primaryDark ml-1">/&gt;</span>
+          </a>
+          <a
+            href="#about"
+            className="block navbar-item hover:text-primary py-2"
+            onClick={closeMenu}
+          >
+            About<span className="textyDark ml-1">/&gt;</span>
+          </a>
+          <a
+            href="#contact"
+            className="block navbar-item hover:text-primary py-2 mb-4"
+            onClick={closeMenu}
+          >
+            Contact<span className="text-primaryDark ml-1">/&gt;</span>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,33 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		fontFamily: {
-			roboto: ['Roboto Condensed', 'sans-serif'],
-			serif: ['Palatino', 'Georgia', 'Times New Roman'],
-			mono: ['Roboto Mono', 'Courier New', 'Courier'],
-		},
-		extend: {
-			colors: {
-				primary: '#f9f9f9',
-				primaryDark: '#494949',
-				primaryGray: '#989e9f',
-				purple: '#5918df',
-				darkPurple: '#14121c',
-				customPink: '#cc2b5e ',
-				customPurple: '#753a88',
-			},
-			backgroundImage: {
-				'start-page': "url('/background4.jpg')",
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto Condensed", "sans-serif"],
+      serif: ["Palatino", "Georgia", "Times New Roman"],
+      mono: ["Roboto Mono", "Courier New", "Courier"],
+      jetbrains: ["JetBrains Mono", "ui-monospace", "monospace"],
+    },
+    extend: {
+      colors: {
+        primary: "#f9f9f9",
+        primaryDark: "#494949",
+        primaryGray: "#989e9f",
+        purple: "#5918df",
+        darkPurple: "#14121c",
+        customPink: "#cc2b5e ",
+        customPurple: "#753a88",
+      },
+      backgroundImage: {
+        "start-page": "url('/background4.jpg')",
+      },
+    },
+  },
+  plugins: [],
 };
 export default config;
